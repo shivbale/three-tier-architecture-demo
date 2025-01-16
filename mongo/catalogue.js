e95aefc7e9363fcf83d8871148771729dd2f4e53
shivbale <104132808+shivbale@users.noreply.github.com>
2025-01-16 21:32:38 +0530

    Update catalogue.js

--- mongo/catalogue.js
+++ mongo/catalogue.js
@@ -3,17 +3,22 @@
 //
 db = db.getSiblingDB('catalogue');
 db.products.insertMany([
-    {sku: 'Watson', name: 'Watson', description: 'Probably the smartest AI on the planet', price: 2001, instock: 2, categories: ['Artificial Intelligence']},
-    {sku: 'Ewooid', name: 'Ewooid', description: 'Fully sentient assistant', price: 200, instock: 0, categories: ['Artificial Intelligence']},
-    {sku: 'HPTD', name: 'High-Powered Travel Droid', description: 'Traveling to the far reaches of the Galaxy? You need this for protection. Comes in handy when you are lost in space', price: 1200, instock: 12, categories: ['Robot']},
-    {sku: 'UHJ', name: 'Ultimate Harvesting Juggernaut', description: 'Extraterrestrial vegetation harvester', price: 5000, instock: 10, categories: ['Robot']},
-    {sku: 'EPE', name: 'Extreme Probe Emulator', description: 'Versatile interface adapter for hacking into systems', price: 953, instock: 1, categories: ['Robot']},
-    {sku: 'EMM', name: 'Exceptional Medical Machine', description: 'Fully automatic surgery droid with exceptional bedside manner', price: 1024, instock: 1, categories: ['Robot']},
-    {sku: 'SHCE', name: 'Strategic Human Control Emulator', description: 'Diplomatic protocol assistant', price: 300, instock: 12, categories: ['Robot']},
-    {sku: 'RED', name: 'Responsive Enforcer Droid', description: 'Security detail, will gaurd anything', price: 700, instock: 5, categories: ['Robot']},
-    {sku: 'RMC', name: 'Robotic Mining Cyborg', description: 'Excellent tunneling capability to get those rare minerals', price: 42, instock: 48, categories: ['Robot']},
-    {sku: 'STAN-1', name: 'Stan', description: 'Observability guru', price: 67, instock: 1000, categories: ['Robot', 'Artificial Intelligence']},
-    {sku: 'CNA', name: 'Cybernated Neutralization Android', description: 'Is your spaceship a bit whiffy? This little fellow will bring a breath of fresh air', price: 1000, instock: 0, categories: ['Robot']}
+    {sku: 'BBQCBR', name: 'BBQCBR', description: 'BBQ Chicken Burrito', price: 245, instock: 10, categories: ['Burrito']},
+    {sku: 'CCHCBR', name: 'CCHCBR', description: 'Chilli Chipotle Chicken Burrito', price: 245, instock: 15, categories: ['Burrito']},
+    {sku: 'CHIPBR', name: 'CHIPBR', description: 'Chipotle Paneer Burrito', price: 235, instock: 15, categories: ['Burrito']},
+    {sku: 'CHFVBR', name: 'CHFVBR', description: 'Chipotle Fajita Veg Burrito', price: 225, instock: 5, categories: ['Burrito']},
+    {sku: 'CPPCBB', name: 'CPPCBB', description: 'Crispy Peri Peri Chicken Burrito Bowl', price: 245, instock: 15, categories: ['BurritoBowl']},
+    {sku: 'CCHCBB', name: 'CCHCBB', description: 'Chilli Chipotle Chicken Burrito Bowl', price: 245, instock: 20, categories: ['BurritoBowl']},
+    {sku: 'BBQPBB', name: 'BBQPBB', description: 'BBQ Paneer Burrito Bowl', price: 235, instock: 10, categories: ['BurritoBowl']},
+    {sku: 'PPPOBB', name: 'PPPOBB', description: 'Peri Peri Potato Burrito Bowl', price: 225, instock: 10, categories: ['BurritoBowl']},
+    {sku: 'CCHCNC', name: 'CCHCNC', description: 'Chilli Chipotle Chicken Nachos', price: 245, instock: 15, categories: ['Nachos']},
+    {sku: 'CHIPNC', name: 'CHIPNC', description: 'Chipotle Paneer Nachos', price: 235, instock: 20, categories: ['Nachos']},
+    {sku: 'CCHCTC', name: 'CCHCTC', description: 'Chilli Chipotle Chicken Tacos', price: 245, instock: 15, categories: ['Tacos']},
+    {sku: 'CHIPTC', name: 'CHIPTC', description: 'Chipotle Paneer Tacos', price: 235, instock: 20, categories: ['Tacos']},
+    {sku: 'PPEPTC', name: 'PPEPTC', description: 'Peri Peri Paneer Tacos', price: 235, instock: 20, categories: ['Tacos']},
+    {sku: 'CMDPSN', name: 'CMDPSN', description: 'Cheese Melt down Paneer Tacos', price: 79, instock: 20, categories: ['Snacks']},
+    {sku: 'AVTSSN', name: 'AVTSSN', description: 'Avacado Tostada', price: 79, instock: 20, categories: ['Snacks']},
+    {sku: 'AVTSSN', name: 'AVTSSN', description: '7 Layer Chicken Taco', price: 79, instock: 20, categories: ['Snacks']}
 ]);
 
 // full text index for searching
